c054679bd9f938f1a9642399b63755e003d715b1
Ashot Israelyan <ashot_israelyan@epam.com>
2019-01-25 14:19:42 +0400

    Styling and credits to API author added.

--- lootcheck/src/components/App.js
+++ lootcheck/src/components/App.js
@@ -8,6 +8,12 @@ const App = () => (
     <Wallet />
     <hr />
     <Loot />
+    <div>
+      Powered by{' '}
+      <a target="_blank" href={'https://www.coindesk.com/price'}>
+        Coindesk
+      </a>
+    </div>
   </div>
 );
 
