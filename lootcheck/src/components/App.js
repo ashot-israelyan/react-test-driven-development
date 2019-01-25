4b5eeba685b1c1c30f942e907f6c77ff9cfc7e6d
Ashot Israelyan <ashot_israelyan@epam.com>
2019-01-25 14:08:43 +0400

    Test coverage report added.

--- lootcheck/src/components/App.js
+++ lootcheck/src/components/App.js
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Wallet from './Wallet';
 import Loot from './Loot';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Loot Check</h2>
-        <Wallet />
-        <hr />
-        <Loot />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <h2>Loot Check</h2>
+    <Wallet />
+    <hr />
+    <Loot />
+  </div>
+);
 
 export default App;
